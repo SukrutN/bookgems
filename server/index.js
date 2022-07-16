078c9adc5fe964707b349fb44f6b0278a31a7203
Sukrut Nadigotti <sukrutnadigotti@gmail.com>
2022-07-15 23:56:47 -0400

    Set up MongoDB connection and start server on specified port

--- server/index.js
+++ server/index.js
@@ -33,3 +33,20 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
