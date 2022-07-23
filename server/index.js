f7b6f884ee7ef4b04700d00eb7ffaabbe94e1339
Sukrut Nadigotti <sukrutnadigotti@gmail.com>
2022-07-23 07:38:36 -0400

    Add user resource routes with authentication

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
@@ -41,6 +42,7 @@ app.post("/auth/register", upload.single("picture"), register);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
