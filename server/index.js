b881e797b93df9cd73a4d287e23dc3a4b9b4661e
Sukrut Nadigotti <sukrutnadigotti@gmail.com>
2022-08-06 20:15:59 -0400

    Finish backend implementation with sample data injection

--- server/index.js
+++ server/index.js
@@ -54,6 +54,7 @@ app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
+mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -63,7 +64,7 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /* ADD DATA ONE TIME */
-    // User.insertMany(users);
-    // Post.insertMany(posts);
+    //User.insertMany(users);
+    //Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
