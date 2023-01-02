67b40d28f59599a40b07eca0bd19c607a8682d49
SukrutN <sukrutnadigotti@gmail.com>
2023-01-01 23:06:05 -0500

    Adjust URL to match deployment

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:5001/assets/${image}`}
+        src={`https://bookgems-backend.onrender.com/assets/${image}`}
       />
     </Box>
   );
