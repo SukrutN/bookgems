fffd25a089a8284edd04c7400e3c7533b86277c3
SukrutN <sukrutnadigotti@gmail.com>
2023-01-02 01:28:53 -0500

    URL re-routing for deployment

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `http://localhost:5001/users/${_id}/${friendId}`,
+      `https://bookgems-backend.onrender.com/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
